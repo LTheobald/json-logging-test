4af465422928f902d19175ada401b2e242694a28
Lee Theobald <lee@ltheobald.co.uk>
2024-02-23 15:15:38 +0000

    Inital commit

---
+++ src/main/java/uk/co/ltheobald/jsonloggingtest/JsonLoggingTestApplication.java
@@ -0,0 +1,13 @@
+package uk.co.ltheobald.jsonloggingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLoggingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonLoggingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/ltheobald/jsonloggingtest/JsonLoggingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.ltheobald.jsonloggingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonLoggingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
